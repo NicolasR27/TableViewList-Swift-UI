cb223c902dcdf981d9026eee92f8b84969bb8c19
Nicolas Rios <nickrios33@yahoo.com>
2019-12-14 15:46:50 -0800

    Initial Commit

---
+++ TableViewList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableViewList
+//
+//  Created by Nicolas Rios on 12/14/19.
+//  Copyright © 2019 Nicolas Rios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
